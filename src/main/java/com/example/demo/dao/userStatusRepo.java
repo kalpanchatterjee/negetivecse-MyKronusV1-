d3e93fdf6eb2840580dfd6ab9056d0001ea65aea
tanmoyhazra <14tanmoy@gmail.com>
2020-12-30 12:13:35 +0530

    testing commit 1

--- src/main/java/com/example/demo/dao/userStatusRepo.java
+++ src/main/java/com/example/demo/dao/userStatusRepo.java
@@ -74,7 +74,7 @@ public class userStatusRepo {
 		SQL.append("  ) AS wData ");
 		SQL.append(" ORDER BY createdTimestamp ASC ");
 
-		System.out.println(SQL.toString());
+//		System.out.println(SQL.toString());
 
 		List<userModel> userStatusList=jdbctm.query(SQL.toString(), BeanPropertyRowMapper.newInstance(userModel.class));
 		System.out.println(lighttpd);
