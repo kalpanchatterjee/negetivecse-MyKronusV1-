ab70c0a3f31a943e3af5a704800795e7508878da
tanmoyhazra <14tanmoy@gmail.com>
2020-12-30 12:37:04 +0530

    testing new

--- src/main/java/com/example/demo/dao/userStatusRepo.java
+++ src/main/java/com/example/demo/dao/userStatusRepo.java
@@ -75,7 +75,7 @@ public class userStatusRepo {
 		SQL.append(" ORDER BY createdTimestamp ASC ");
 
 		System.out.println(SQL.toString());
-		System.out.println("commiting No!!!!!!!!");
+		System.out.println("commiting Yes!!!!!!!!");
 
 		List<userModel> userStatusList=jdbctm.query(SQL.toString(), BeanPropertyRowMapper.newInstance(userModel.class));
 		System.out.println(lighttpd);
